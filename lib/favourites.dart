9f2d9ce3c35314758b648643268c238138f744c7
yatharthranjan <yatharthranjan89@gmail.com>
2019-09-20 17:54:39 +0100

    add swipe to remove from favourites

--- lib/favourites.dart
+++ lib/favourites.dart
@@ -40,67 +40,88 @@ class FavouritesState extends State<Favourites> {
       _favourites = stringPokemons.map((sp) {
         dynamic p = json.decode(sp);
         return Pokemon(
-          p["name"],
-          FadeInImage.assetNetwork(
-            placeholder: "images/pokeball_placeholder.gif",
-            image: p["thumbnailUrl"],
-            imageScale: 0.30,
-            placeholderScale: 2.5,
-            height: 300,
-          ),
-          p["height"],
-          p["weight"]);}).toList();
+            p["id"],
+            p["name"],
+            FadeInImage.assetNetwork(
+              placeholder: "images/pokeball_placeholder.gif",
+              image: p["thumbnailUrl"],
+              imageScale: 0.30,
+              placeholderScale: 2.5,
+              height: 300,
+            ),
+            p["height"],
+            p["weight"]);
+      }).toList();
     });
   }
 
   Widget _buildFavourites() {
     getAllPokemons();
     return ListView.builder(
-        padding: const EdgeInsets.all(16.0),
-        itemBuilder: (context, i) {
-          if (i.isOdd) return Divider();
-
-          final index = i ~/ 2;
-          if (index < _favourites.length) return _buildRow(_favourites[index]);
-          return null;
-        });
+      padding: const EdgeInsets.all(16.0),
+      itemBuilder: (context, i) {
+        if (i < _favourites.length) return _buildRow(_favourites[i]);
+        return null;
+      },
+    );
   }
 
   Widget _buildRow(Pokemon pokemon) {
-    return ListTile(
-      leading: pokemon.thumbnail,
-      title: Text(
-        pokemon.name,
-        style: _biggerFont,
-      ),
-      trailing: Icon(
-        Icons.favorite,
-        color: Colors.red,
-      ),
-      subtitle: Row(children: <Widget>[
-        Text("Weight: ${pokemon.weight},  "),
-        Text("Height: ${pokemon.height}"),
-      ]),
-      onTap: () {
-        setState(() {
-//          if (alreadySaved) {
-//            _saved.remove(wordPair);
-//          } else {
-//            _saved.add(wordPair);
-//          }
-        });
-      },
-    );
+    return Builder(builder: (BuildContext context) {
+      return Dismissible(
+          // Show a red background as the item is swiped away.
+          background: Container(color: Colors.red),
+          key: Key(pokemon.name),
+          onDismissed: (direction) {
+            removeFromFavourites(pokemon);
+            setState(() {
+              _favourites.remove(pokemon);
+            });
+
+            Scaffold.of(context).showSnackBar(SnackBar(
+              content: Text("${pokemon.name} removed from favourites"),
+            ));
+          },
+          child: Column(children: <Widget>[
+            ListTile(
+              leading: pokemon.thumbnail,
+              title: Text(
+                pokemon.name,
+                style: _biggerFont,
+              ),
+              trailing: Icon(
+                Icons.favorite,
+                color: Colors.red,
+              ),
+              subtitle: Row(children: <Widget>[
+                Text("Weight: ${pokemon.weight},  "),
+                Text("Height: ${pokemon.height}"),
+              ]),
+            ),
+            Divider()
+          ]));
+    });
+  }
+
+  Future<void> removeFromFavourites(Pokemon pokemon) async {
+    int favCount;
+    try {
+      favCount = await _platform.invokeMethod(
+          'removeFromFavourites', {"id": pokemon.id, "name": pokemon.name});
+    } on PlatformException catch (e) {
+      Logging.log(_TAG, "There was an exception removing from favourites : $e");
+    }
   }
 }
 
 class Pokemon {
+  int _id;
   String _name = "";
   FadeInImage _thumbnail;
   int _height = 0;
   int _weight = 0;
 
-  Pokemon(this._name, this._thumbnail, this._height, this._weight);
+  Pokemon(this._id, this._name, this._thumbnail, this._height, this._weight);
 
   int get weight => _weight;
 
@@ -109,4 +130,20 @@ class Pokemon {
   FadeInImage get thumbnail => _thumbnail;
 
   String get name => _name;
+
+  int get id => _id;
+
+  @override
+  bool operator ==(Object other) =>
+      identical(this, other) ||
+      other is Pokemon &&
+          runtimeType == other.runtimeType &&
+          _id == other._id &&
+          _name == other._name &&
+          _height == other._height &&
+          _weight == other._weight;
+
+  @override
+  int get hashCode =>
+      _id.hashCode ^ _name.hashCode ^ _height.hashCode ^ _weight.hashCode;
 }
